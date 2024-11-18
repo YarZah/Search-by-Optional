392fee75fe5a88cb834c15325e1180152e71ed4a
YarZah <jarikzagreb16@gmail.com>
2024-11-18 18:12:35 +0100

    Optional-search

--- src/main/java/com/gmail/jarikzagreb16/App.java
+++ src/main/java/com/gmail/jarikzagreb16/App.java
@@ -11,5 +11,15 @@ import static com.gmail.jarikzagreb16.Option.getElement;
  */
 public class App 
 {
+    public static void main( String[] args ) {
+        int[] arr = {20, 60, 15, 90, 46, 69, 26, 47, 124};
 
+        Optional<Integer> existing = getElement(arr, 7);
+        existing.ifPresentOrElse(value -> System.out.println("7 індекс =" + value),
+                () -> System.out.println("7 індекс не знайдено."));
+        Optional<Integer> missing = getElement(arr, 25);
+        missing.ifPresentOrElse(value -> System.out.println("25 індекс =" + value),
+                () -> System.out.println("25 індекс не знайдено."));
+    }
 }
+
