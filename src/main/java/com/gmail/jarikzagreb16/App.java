a1fd3a716769799c48d9d05a3476dd3dada6a895
YarZah <jarikzagreb16@gmail.com>
2024-11-18 18:09:36 +0100

    main

---
+++ src/main/java/com/gmail/jarikzagreb16/App.java
@@ -0,0 +1,15 @@
+package com.gmail.jarikzagreb16;
+
+import javax.swing.*;
+import java.util.Optional;
+
+import static com.gmail.jarikzagreb16.Option.getElement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+}
---
+++ src/main/java/com/gmail/jarikzagreb16/Option.java
@@ -0,0 +1,7 @@
+package com.gmail.jarikzagreb16;
+
+import java.util.Optional;
+
+public class Option {
+
+}
